2663e06ba654064a613ae61a48d6daedf54fe8d8
Mark Meysenburg <mark.meysenburg@doane.edu>
2023-04-17 08:35:44 -0500

    Updated test.sh for spring 2023 replit config

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar -d . *.java 
-java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar SpellCheckTest
+javac -classpath .:target/dependency/* -d . $(find . -type f -name '*.java')
+java -classpath .:target/dependency/* SpellCheckTest
